04c2153edc723c42aa2dfd1ea3b76aa7b168c960
shubhamchandak94 <schandak@stanford.edu>
2016-12-16 16:54:43 -0800

    Update matchsortnoisy8.py

--- python/noisy/matchsortnoisy8.py
+++ python/noisy/matchsortnoisy8.py
@@ -1,13 +1,10 @@
-from itertools import imap
-import operator
+#Reordering for noisy reads without RC. Similar to v2 in report without RC. Uses five dictionaries (ind) and a majority
+#based reference.
+#Note that here reads with 'N' are supported.
+
+#Worked really well for simulated noise without RC. Slightly worse with real data due to RC.
+
 from distance import hamming
-# hamming2 function from http://code.activestate.com/recipes/499304-hamming-distance/
-def hamming2(str1, str2):
-    #assert len(str1) == len(str2)
-    #ne = str.__ne__  ## this is surprisingly slow
-    ne = operator.ne
-    return sum(imap(ne, str1, str2))
-#	return sum([str1[i]!=str2[i] for i in range(len(str1))])	
 
 def char2index(c):
 	if c == 'A':
@@ -106,25 +103,6 @@ while True:
 			ref = findmajority(count)	
 			break
 
-#if len(d[lines[current][0:matchlen]]) == 0:
-#		del d[lines[current][0:matchlen]]
-#	else:
-#		for i in d[lines[current][0:matchlen]]:
-#			if hamming2(lines[current][matchlen:],lines[i][matchlen:]) <= thresh:
-#				current = i
-#				flag = 1
-#				break
-#	if flag == 1:
-#		continue
-#	for j in range(1,maxmatch):
-#		if lines[current][j:j+matchlen] in d:
-#			for i in d[lines[current][j:j+matchlen]]:
-#				if hamming2(lines[current][j+matchlen:],lines[i][matchlen:readlen-j]) <= thresh:
-#					current = i
-#					flag = 1
-#					break
-#			if flag == 1:
-#					break
 	if flag == 1:
 		continue
 	current = remainingreads.pop()
