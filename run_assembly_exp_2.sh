b9c092210a29d132cd21ca20630bc69d923e3b02
Kedar Tatwawadi <cslcollab-kedart@idoia-work-01.csl.illinois.edu>
2017-09-27 21:54:28 -0500

    update script to run assembly experiments

--- run_assembly_exp_2.sh
+++ run_assembly_exp_2.sh
@@ -12,23 +12,40 @@ mkdir $tmp_folder
 denoising_stats_log=$log_folder/overall_denoising_stats.log_file
 
 ##### TOOLS to use
-spades_tool=~/code/assembly/SPAdes-3.11.0-Linux/bin/spades.py
-quast_tool=~/code/assembly/quast-4.5/quast.py
+spades_tool=../SPAdes-3.11.0-Linux/bin/spades.py
+quast_tool=../quast-4.5/quast.py
 
 
 fastq_file=$prefix.fastq
-harc_compressed_file=$prefix.harc
+harc_compressed_file=$prefix".harc"
 HARC_fastq_1=$prefix"_1.HARC.fastq"
 HARC_fastq_2=$prefix"_2.HARC.fastq"
 
+## Run Original File
+python $spades_tool -t 16 --only-assembler -1 $prefix"_1.fq" -2 $prefix"_2.fq" -o $tmp_folder
+quast_output=$log_folder/quastreport_original_only_assembler
+python $quast_tool $tmp_folder/contigs.fasta -R $ref_fasta -o $quast_output --plots-format pdf --labels original_only_assembler
 
+## Run Original File with spades_denoising
+python $spades_tool -t 16  -1 $prefix"_1.fq" -2 $prefix"_2.fq" -o $tmp_folder
+quast_output=$log_folder/quastreport_original_with_spades_denoising
+python $quast_tool $tmp_folder/contigs.fasta -R $ref_fasta -o $quast_output --plots-format pdf --labels original_with_spades_denoising
+
+## Run On Clean file
+python util/combine_into_fastq_clean.py $prefix"_1" 1
+python util/combine_into_fastq_clean.py $prefix"_2" 2
+python $spades_tool -t 16 --only-assembler -1 $prefix"_1.clean.fastq" -2 $prefix"_2.clean.fastq" -o $tmp_folder
+quast_output=$log_folder/quastreport_clean
+python $quast_tool $tmp_folder/contigs.fasta -R $ref_fasta -o $quast_output --plots-format pdf --labels clean
+
+#Run on different parameters of HARC
 for n in 0.05 0.1 0.2
 do
     for r in 2 4
     do
         for a in 2 4 8
         do
-            ./harc -c $fastq_file -pq -r $r -a $a -n $n
+            ./harc -c $fastq_file -pq -r $r -a $a -n $n -t 16
             ./harc -d $harc_compressed_file -p
             param_string=_n$n._r$r._a$a
             python get_denoising_stats.py $prefix $log_folder/$param_string  >> $denoising_stats_log            
@@ -41,7 +58,7 @@ do
             mv $tmp_folder/xaa $HARC_fastq_1
             mv $tmp_folder/xab $HARC_fastq_2
 
-            python $spades_tool -t 8 --only-assembler -1 $HARC_fastq_1 -2 $HARC_fastq_2 -o $tmp_folder
+            python $spades_tool -t 16 --only-assembler -1 $HARC_fastq_1 -2 $HARC_fastq_2 -o $tmp_folder
 
             quast_output=$log_folder/quastreport_$param_string
             python $quast_tool $tmp_folder/contigs.fasta -R $ref_fasta -o $quast_output --plots-format pdf --labels $param_string
--- util/combine_into_fastq_clean.py
+++ util/combine_into_fastq_clean.py
@@ -5,21 +5,20 @@ from tqdm import tqdm
 from itertools import izip
 
 basename = sys.argv[1]
+pair=sys.argv[2]
 infile_reads = basename + ".clean"
-infile_quality = basename + ".clean.quality"
-infile_readid = basename + ".id"
 outfile_fastq = basename + ".clean.fastq"
+readlen = 100
 
 f_reads = open(infile_reads,'r')
-f_quality = open(infile_quality, 'r')
-f_readid = open(infile_readid, 'r')
 f_out_fastq = open(outfile_fastq,'w')
 
 separator = "+\n"
 
-
-for readid,read, quality in izip(f_readid, f_reads, f_quality):
-	f_out_fastq.write(readid) 
+for i,read in enumerate(f_reads):
+	readid = "@read_" + str(i)+"/" + pair + "\n"
+	quality= "I"*(len(read)-1) + "\n"
+	f_out_fastq.write(readid)
 	f_out_fastq.write(read)
 	f_out_fastq.write(separator)
 	f_out_fastq.write(quality)
